a68ded8c86e87c89b15bb3d8a9d22fba0880ef4a
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-04-10 18:01:28 -0300

    add: new project and dark-mode

--- js/script.js
+++ js/script.js
@@ -30,3 +30,44 @@ window.addEventListener("scroll", function() {
 window.onload = () => {
     topo.classList.toggle ("hide-top");
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    const mainSection = document.getElementById('home');
+    const toggleButton = document.getElementById("toggle-theme");
+    const icon = toggleButton.querySelector("i");
+
+    const savedTheme = localStorage.getItem("theme");
+    const isDark = savedTheme === "dark";
+
+    if (isDark) {
+        document.documentElement.classList.add("dark");
+        document.body.classList.add("dark-mode");
+        icon.classList.add("bx-sun");
+        icon.classList.remove("bx-moon");
+        mainSection.style.backgroundImage = "url('/img/background-dark.png')";
+    } else {
+        document.body.classList.remove("dark-mode");
+        icon.classList.add("bx-moon");
+        icon.classList.remove("bx-sun");
+        mainSection.style.backgroundImage = "url('/img/background-light.jpg')";
+    }
+
+    const toggleTheme = () => {
+        const isNowDark = document.documentElement.classList.toggle("dark");
+        document.body.classList.toggle("dark-mode");
+        icon.classList.toggle("bx-sun");
+        icon.classList.toggle("bx-moon");
+
+        if (isNowDark) {
+            mainSection.style.backgroundImage = "url('/img/background-dark.png')";
+            localStorage.setItem("theme", "dark");
+        } else {
+            mainSection.style.backgroundImage = "url('/img/background-light.jpg')";
+            localStorage.setItem("theme", "light");
+        }
+    };
+
+    if (toggleButton) {
+        toggleButton.addEventListener("click", toggleTheme);
+    }
+});
